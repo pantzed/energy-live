49b63c4931340f6bb67866e783aca8b5e9baccc7
Edward <edpantzar@gmail.com>
2018-05-25 16:25:18 -0500

    Added xml to json parser

--- app.js
+++ app.js
@@ -1,8 +1,74 @@
 (function(){
-  fetch("http://egauge8642.egaug.es/cgi-bin/egauge?tot")
-  .then((response) => response.text())
-  // .then((response) => response.text())
-  .then((data) => {
-    console.log(data)
+let jsonObject;
+let registers;
+let timeStamp;
+let serial;
+
+function makeRegistersObject(registers) {
+  let obj = {};
+  registers.forEach(function(innerObj) {
+    obj[innerObj['@attributes'].n] = {'type': `${[innerObj['@attributes'].t]}`, 'power': `${[innerObj.v]}`};
   });
+  return obj;
+}
+
+// Changes XML to JSON
+// Modified version from here: http://davidwalsh.name/convert-xml-json
+function xmlToJson(xml) {
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+	// do children
+	// If just one text node inside
+	if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
+		obj = xml.childNodes[0].nodeValue;
+	}
+	else if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+}
+
+function callEgauge() {
+  fetch("https://cors-anywhere.herokuapp.com/http://egauge8642.egaug.es/cgi-bin/egauge?tot", {
+    method: "GET"
+  })
+  .then(data => data.text())
+  .then(xml => {
+    xmlDOM = new DOMParser().parseFromString(xml, 'text/xml');
+    jsonObject = xmlToJson(xmlDOM);
+    timeStamp = jsonObject.data.ts;
+    serial = jsonObject.data['@attributes'].serial;
+    registers = makeRegistersObject(jsonObject.data.r);
+    console.log(registers);
+  })
+}
+
+callEgauge();
+
 })();
